bd3ad29d71e82b77d64b5c2202f82b2162c18bda
masautn <matiasa@frba.utn.edu.ar>
2022-08-16 23:59:37 -0300

    final inital commit

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 
-
+let mode = 'black'
 function init(rows , cols){
 
 
@@ -22,9 +22,51 @@ gridContainer.style.setProperty('--grid-cols', cols);
 function changeColor(e){
 
     switch (mode){
-        
+        case "black":
+            e.target.style.backgroundColor = "black";
+            break;
+        case "rainbow":
+            e.target.style.backgroundColor = rainbowColor();
+            break;
+        case "erase":
+            e.target.style.backgroundColor = "gray";
+            break;   
     }
-    e.target.style.backgroundColor = "black"
+    
+
+}
+
+function rainbowColor(){
+ console.log(mode);
+ 
+ const red = getRandomInt(254);
+ const green = getRandomInt(254);
+ const blue = getRandomInt(254);
+
+ console.log(red);
+ console.log(green);
+ console.log(blue);
+ return (`rgb( ${red}, ${green}, ${blue}`)
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function changeMode(e){
+    switch(e.target.id){
+
+        case "blackBtn":
+            mode = "black";
+            break;
+        case "rainbowBtn":
+            mode = "rainbow";
+            break; 
+        case "eraserBtn":
+            mode = "erase";
+            break;    
+    }
 
 }
 
@@ -35,4 +77,27 @@ function changeColor(e){
 
 
 
+
+
 init(16, 16);
+
+
+const blackBtn = document.getElementById("blackBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+
+blackBtn.addEventListener('click', changeMode );
+rainbowBtn.addEventListener('click', changeMode );
+eraserBtn.addEventListener('click', changeMode );
+resetBtn.addEventListener('click', resetGrid );
+
+
+
+function resetGrid(){
+    let subgrid = document.querySelectorAll("#subgrid");
+    subgrid.forEach(element => {
+        element.style.backgroundColor = "gray";
+    });
+}
